32e7909cb3a32f98172397872bc8cb036be0fdee
arseniiarsenii <arseniivelichko2@gmail.com>
2021-04-21 09:14:41 +0300

    added logging

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, request, Response
 from flask_restful import Api, Resource
 import typing as tp
 import requests
+import logging
+
+# set up logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="backend.log",
+    format="%(asctime)s %(levelname)s: %(message)s"
+)
+
+logging.info('Backend started')
 
 # global variables
 agent_api_address: str = "http://127.0.0.1:8080/api"
@@ -15,6 +25,7 @@ api = Api(app)
 def update_agent_state(priority: int) -> int:
     """post an updated system state to the agent to keep it synced with the local state"""
 
+    logging.info(f"Changing agent state to {current_state}")
     change_agent_state = requests.post(
         url=f"{agent_api_address}/state-update",
         json={
@@ -31,6 +42,10 @@ class CurrentState(Resource):
     """returns current state of the system to the frontend as it is long polling the backend"""
 
     def get(self) -> tp.Dict[str, int]:
+        logging.debug(
+            f"Received a request for current state. Responding with {current_state}"
+        )
+
         response = {"state_no": current_state}
         return response
 
@@ -39,6 +54,9 @@ class FormHandler(Resource):
     """accepts a filled form from the frontend and reroutes it to the agent"""
 
     def post(self) -> int:
+        logging.info(
+            f"Received a form. Relaying to the agent."
+        )
 
         # parse the form data
         form_data = request.get_json()
@@ -49,11 +67,26 @@ class FormHandler(Resource):
             json=form_data
         )
 
+        global current_state
+
         # change own state and tell the agent to change it's state
         if relay_the_form.ok:
-            global current_state
             current_state = 2
             update_agent_state(priority=2)
+
+            logging.info(
+                f"Form relay success. Current state: {current_state}"
+            )
+
+        else:
+            logging.error(
+                f"""
+                Form relay failed. 
+                Form: {form_data},
+                Request status: {relay_the_form.status_code}
+                Current state: {current_state}
+                """
+            )
 
         return 200
 
@@ -62,20 +95,33 @@ class StateUpdateHandler(Resource):
     """handles a state update request"""
 
     def post(self) -> int:
+        logging.info(
+            f"Received a request to update the state."
+        )
+
         # parse the request data
         data = request.get_json()
 
         # validate the request
         global valid_states
+        global current_state
+
         if not data["change_state_to"] in valid_states:
+            logging.warning(
+                f"Invalid state transition: '{data['change_state_to']}' is not a valid state. Staying at {current_state}"
+            )
+
             return Response(
                 response='{"status": 406, "msg": "invalid state"}',
                 status=406
             )
 
         # change own state to the one specified by the sender
-        global current_state
         current_state = data["change_state_to"]
+
+        logging.info(
+            f"Successful state transition to {data['change_state_to']}"
+        )
 
         return 200
 
@@ -89,6 +135,8 @@ api.add_resource(StateUpdateHandler, "/api/state-update")
 @app.route("/")
 def index():
     """returns an index page to the client"""
+
+    logging.info("Index page served")
 
     return "Index page here"
 
