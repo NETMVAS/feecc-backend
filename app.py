0a65418087f10e2f12bb583e0420358d6eec5efa
arseniiarsenii <arseniivelichko2@gmail.com>
2021-04-26 16:33:50 +0300

    added frontend files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 from flask_restful import Api, Resource
 import typing as tp
 import requests
@@ -138,8 +138,8 @@ def index():
 
     logging.info("Index page served")
 
-    return "Index page here"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=8080)
